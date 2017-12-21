e1eef1f90990ce4745d376dc14a93ad783bd9373
Timothé <noki.dev@gmail.com>
2017-12-21 01:26:40 +0100

    Adding Effect Manager
    3 Previous commits don't works without it.

---
+++ BattleBall/Assets/Effects/EffectManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectManager : MonoBehaviour {
+
+    private Power currentPower;
+
+    public void ActivateCurrentPower()
+    {
+        if(currentPower != null && currentPower.powerActivator == Power.PowerActivator.MANUAL)
+        {
+            currentPower.StartEffect();
+        }
+    }
+
+    public void SetPower(Power power)
+    {
+        if(currentPower != null)
+        {
+            Destroy(currentPower.gameObject);
+        }
+        currentPower = power;
+        currentPower.OnEnd += OnPowerEnd;
+        if(currentPower.powerActivator == Power.PowerActivator.AUTOMATIC)
+        {
+            currentPower.StartEffect();
+        }
+    }
+
+    private void OnPowerEnd()
+    {
+        currentPower.OnEnd -= OnPowerEnd;
+        currentPower = null;
+    }
+}
